87c3bede65f1feb6ac19cff3bd6296fd8b20a37a
JuliaDaniR <julidrz@hotmail.com.ar>
2024-06-08 11:09:17 -0300

    Agregando cambio

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 // Esperar a que el DOM esté completamente cargado
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener("DOMContentLoaded", function () {
   // Obtener el formulario por su ID
-  const form = document.getElementById('encuestaForm');
+  const form = document.getElementById("encuestaForm");
 
   // Agregar el evento 'submit' al formulario
-  form.addEventListener('submit', function(event) {
+  form.addEventListener("submit", function (event) {
     event.preventDefault(); // Evitar que el formulario se envíe de forma predeterminada
 
     const mensaje = obtenerRespuestas();
@@ -20,34 +20,38 @@ document.addEventListener('DOMContentLoaded', function() {
     const respuestas = [];
     const inputs = document.querySelectorAll('input[type="text"]');
 
-    inputs.forEach(function(input, index) {
+    inputs.forEach(function (input, index) {
       const numeroRespuesta = index + 1; // Obtener el número de la respuesta
-      respuestas.push(`${numeroRespuesta}. ${input.previousElementSibling.textContent} ${input.value}`);
+      respuestas.push(
+        `${numeroRespuesta}. ${input.previousElementSibling.textContent} ${input.value}`
+      );
     });
-    
-    return respuestas.join('\n');
+
+    return respuestas.join("\n");
   }
 
   // Función para enviar el mensaje por WhatsApp
   function enviarWhatsapp(mensaje) {
-    const numeroComercio = '3413507933'; // Aquí colocas el número del comercio
-    const url = `https://wa.me/${numeroComercio}?text=${encodeURIComponent(mensaje)}`;
-    window.open(url, '_blank');
+    const numeroComercio = `${numeroTelefono}`; // Aquí colocas el número del comercio
+    const url = `https://wa.me/${numeroComercio}?text=${encodeURIComponent(
+      mensaje
+    )}`;
+    window.open(url, "_blank");
   }
 
   // Función para mostrar mensaje de agradecimiento
   function mostrarAgradecimiento() {
     // Crear elemento de mensaje
-    const mensajeDiv = document.createElement('div');
-    mensajeDiv.textContent = '¡Gracias por enviar la encuesta!';
+    const mensajeDiv = document.createElement("div");
+    mensajeDiv.textContent = "¡Gracias por enviar la encuesta!";
 
     // Agregar estilos al mensaje
-    mensajeDiv.style.padding = '10px';
-    mensajeDiv.style.backgroundColor = '#fcb045';
-    mensajeDiv.style.color = 'black';
-    mensajeDiv.style.borderRadius = '5px';
-    mensajeDiv.style.width = '500px';
-    mensajeDiv.style.margin = '20px auto';
+    mensajeDiv.style.padding = "10px";
+    mensajeDiv.style.backgroundColor = "#fcb045";
+    mensajeDiv.style.color = "black";
+    mensajeDiv.style.borderRadius = "5px";
+    mensajeDiv.style.width = "500px";
+    mensajeDiv.style.margin = "20px auto";
 
     // Insertar el mensaje después del formulario
     form.parentNode.insertBefore(mensajeDiv, form.nextSibling);
